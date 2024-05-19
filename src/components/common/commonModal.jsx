17c45646160cd88b58ed7e5ebcfb04d40b50d232
이명필 <zkwmdi30@gmail.com>
2024-05-19 21:52:51 +0900

    공통 모달 추가 (스토어 연결 필요)

---
+++ src/components/common/commonModal.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Modal from 'react-modal';
+
+export default function CommonModal() {
+  const customModalStyles = {
+    overlay: {
+      backgroundColor: ' rgba(0, 0, 0, 0.4)',
+      width: '100%',
+      height: '100vh',
+      zIndex: '10',
+      position: 'fixed',
+      top: '0',
+      left: '0'
+    },
+    content: {
+      width: '360px',
+      height: '180px',
+      zIndex: '150',
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+      borderRadius: '10px',
+      boxShadow: '2px 2px 2px rgba(0, 0, 0, 0.25)',
+      backgroundColor: 'white',
+      justifyContent: 'center',
+      overflow: 'auto'
+    }
+  };
+
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
+  <Modal isOpen={modalIsOpen} onRequestClose={() => setIsOpen(false)} style={customModalStyles} shouldCloseOnOverlayClick ariaHideApp={false}>
+    <h2>언어 선택</h2>
+    <button type="button" onClick={() => setIsOpen(false)}>
+      close
+    </button>
+  </Modal>;
+}
