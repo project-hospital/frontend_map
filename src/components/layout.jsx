8219936c4b0456418021e83aabb7db2289baee29
이명필 <zkwmdi30@gmail.com>
2024-05-19 13:06:49 +0900

    상단 로고 추가 및 레이아웃 일부 변경

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import MainContents from './mainContents/mainContents';
+// import googleLogo from '../assets/google.png';
+import kakaoLogo from '../assets/kakao.png';
+import facebookLogo from '../assets/facebook.png';
+import googleLogo from '/assert/google.png';
 
 function Layout() {
   return (
     <div className="flex flex-col h-screen">
-      <header className="flex flex-col flex-1">
-        <div className="flex justify-center items-center text-3xl font-bold h-16">헤더</div>
+      <section className="flex flex-col flex-1">
         <MainContents />
-        <div className="flex justify-center items-center text-3xl font-bold h-16">푸터</div>
-      </header>
+      </section>
+
+      {/* <div className="flex justify-center items-center text-3xl font-bold h-16">푸터</div> */}
+
+      <div className="fixed top-2 right-2 flex space-x-8 mt-8 mr-8">
+        <img src={googleLogo} alt="google_logo" className="w-8 h-auto" />
+        <img src={kakaoLogo} alt="kakao_logo" className="w-8 h-auto" />
+        <img src={facebookLogo} alt="facebook_logo" className="w-8 h-auto" />
+      </div>
     </div>
   );
 }
