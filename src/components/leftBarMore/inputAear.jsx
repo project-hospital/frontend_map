18c320ab15f411ca3c1ecd7cfb0f932213786bd6
이명필 <zkwmdi30@gmail.com>
2024-08-15 21:45:37 +0900

    reviewText 데이터 연결

--- src/components/leftBarMore/inputAear.jsx
+++ src/components/leftBarMore/inputAear.jsx
@@ -13,10 +13,17 @@ export default function InputAear() {
   const clickRating = (score) => {
     setRating(score);
   };
+  const [reviewText, setReviewText] = useState('');
+  const reviewTyping = (e) => {
+    setReviewText(e.target.value);
+  };
 
   const [, setOpenThanksPopup] = useRecoilState(specialThanksState);
-  const closeThanksPopup = () => {
-    setOpenThanksPopup(true);
+  const submitReview = () => {
+    if (reviewText !== '') {
+      setOpenThanksPopup(true);
+      setReviewText('');
+    }
   };
 
   return (
@@ -108,8 +115,14 @@ export default function InputAear() {
         <textarea
           className="border border-gray-300 rounded w-full p-2 h-24 resize-none"
           placeholder="작성 후 30분 뒤 암호화되어 삭제하실 수 없습니다."
+          onChange={reviewTyping}
+          value={reviewText}
         />
-        <button type="button" className="mt-2 w-full bg-blue-500 text-white py-2 rounded" onClick={closeThanksPopup}>
+        <button
+          type="button"
+          className={`mt-2 w-full ${reviewText === '' ? 'bg-gray-400' : 'bg-blue-500'} text-white py-2 rounded`}
+          onClick={submitReview}
+        >
           등록
         </button>
       </div>
